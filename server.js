d8ddf723dbe955c9b79e0eef2a33428034d06617
AkshayTannirwar <tannirwarakshay@gmail.com>
2017-08-10 19:39:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one',function(req,res)
 {
-   res.send('Article one request and you will be served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res)

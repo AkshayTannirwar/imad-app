675e688666aa3c7e55efd1ca8c86f05e1591a4c6
AkshayTannirwar <tannirwarakshay@gmail.com>
2017-08-14 18:32:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,25 @@ button.onclick = function(){
     request.open('GET','http://tannirwarakshay.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+//Submit Name
+
+
+var inputName = document.getElementById('name');
+var name = inputName.value;
+var submit = document.getElementById('submit-btn');
+submit.onclick = function() {
+    
+    //Make a request to server and send the name
+    //capture a list of naame and render it as a list
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for(var i=0; i < names.length; i++){
+        list += '<li>' + names[i] + '<li>';
+    }
+    
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+    
+}
